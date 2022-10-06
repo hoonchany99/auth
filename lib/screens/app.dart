b17c7327c7628781104fcdb4808029b72ab6b982
윤훈찬 <hoonchan@yunhunchan-ui-MacBookPro.local>
2022-10-06 22:11:44 +0900

    Auth and Store done

--- lib/screens/app.dart
+++ lib/screens/app.dart
@@ -1,17 +1,23 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 
 class App extends StatelessWidget {
   final _firebaseAuth = FirebaseAuth.instance;
+
   final _emailController = TextEditingController();
   final _passwordController = TextEditingController();
+
+  final _fireStore = FirebaseFirestore.instance;
+
+  final _contextController = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Auth'),
+        title: Text('Auth & Store'),
       ),
       body: Center(
         child: Column(
@@ -24,7 +30,12 @@ class App extends StatelessWidget {
               controller: _passwordController,
               decoration: const InputDecoration(labelText: 'Password'),
             ),
-            ElevatedButton(onPressed: Register, child: Text('Register'))
+            ElevatedButton(onPressed: Register, child: Text('Register')),
+            TextFormField(
+              controller: _contextController,
+              decoration: const InputDecoration(labelText: 'Context'),
+            ),
+            ElevatedButton(onPressed: Boxify, child: Text('Boxify'))
           ],
         ),
       ),
@@ -53,6 +64,41 @@ class App extends StatelessWidget {
 
       _emailController.text = '';
       _passwordController.text = '';
+    } catch (e) {
+      print(e);
+      Get.snackbar('Error', e.toString(), snackPosition: SnackPosition.BOTTOM);
+    }
+  }
+
+  Future<void> Boxify() async {
+    try {
+      final user =
+          _fireStore.collection('box').add({'name': _contextController.text});
+
+      if (user != null) {
+        print('Boxify Success');
+      } else {
+        print('Failed');
+      }
+      _contextController.text = '';
+    } catch (e) {
+      print(e);
+      Get.snackbar('Error', e.toString(), snackPosition: SnackPosition.BOTTOM);
+    }
+    Getify();
+  }
+
+  Future<void> Getify() async {
+    try {
+      final user =
+          await _fireStore.collection('box').doc("ORlPgwMsc8brvRKmgfNK").get();
+
+      if (user != null) {
+        print("Getify Success");
+        print(user.data()!['name']);
+      } else {
+        print('Failed');
+      }
     } catch (e) {
       print(e);
       Get.snackbar('Error', e.toString(), snackPosition: SnackPosition.BOTTOM);
